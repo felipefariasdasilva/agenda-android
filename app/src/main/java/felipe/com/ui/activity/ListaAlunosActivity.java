9ff11a887584d0debff3a177c53207e872ff4cad
felipefariasdasilva <feliipefarias@outlook.com>
2020-04-30 18:00:43 -0300

    add new student form activity

--- app/src/main/java/felipe/com/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/felipe/com/ui/activity/ListaAlunosActivity.java
@@ -1,10 +1,8 @@
-package felipe.com;
+package felipe.com.ui.activity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,17 +11,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import felipe.com.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
+        setTitle("Lista de Alunos");
 
         List<String> alunos = new ArrayList<>(Arrays.asList("felipe", "fran", "jose", "maria", "ana"));
 
-        ListView listaAlunos = findViewById(R.id.activity_main_lista_alunos);
+        ListView listaAlunos = findViewById(R.id.activity_lista_alunos_listview);
 
         listaAlunos.setAdapter(
                 new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
