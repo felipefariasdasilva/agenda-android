7651ac47f64ec48d66bfb696d0b8f45487647afb
felipefariasdasilva <feliipefarias@outlook.com>
2020-04-30 16:54:50 -0300

    added listview and floating action button

--- app/src/main/java/felipe/com/MainActivity.java
+++ app/src/main/java/felipe/com/MainActivity.java
@@ -1,17 +1,33 @@
 package felipe.com;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Toast.makeText(this, "FELIPE FARIAS", Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("felipe", "fran", "jose", "maria", "ana"));
+
+        ListView listaAlunos = findViewById(R.id.activity_main_lista_alunos);
+
+        listaAlunos.setAdapter(
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
+
+
     }
 }
